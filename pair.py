72be160f6d901c04bc0e614714ffb7178b7e92c2
Patrick Hall <pat@amundo.com>
2009-08-28 16:21:01 -0700

    initial dump

---
+++ pair.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8 
+import re
+
+pair = u"""Khe na:ta hi:baya ?em mens’in khe nata ?ima:ta law t.’i ?acho khe qahtín shudima:du.
+My daughter and son brought with them their friends.""".splitlines()
+
+
+def tokenize(sentence):
+  whitespaceRE = 
+  return sentence.split()
+
+
